aed0ce57c805243bdc9f42a461e73ba918581b0c
Deivd Francis <deividfrancisdeoliveira@gmail.com>
2017-05-10 04:50:22 -0400

    ediçao da tabela

--- conteudo/cont-matematica.php
+++ conteudo/cont-matematica.php
@@ -4,6 +4,38 @@
         <h3 class="titulo" id="tema-1"><?php echo $mat[1] ?></h3>
         <h3 class="titulo" id="tema-2"><?php echo $mat[2] ?></h3>
         <h3 class="titulo" id="tema-3"><?php echo $mat[3] ?></h3>
+          <table>
+            <thead>
+              <tr>
+                <th>p</th>
+                <th>q</th>
+                <th>p --> q</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>V</td>
+                <td>V</td>
+                <td>V</td>
+              </tr>
+              <tr>
+                <td>V</td>
+                <td>V</td>
+                <td>F</td>
+              </tr>
+              <tr>
+                <td>V</td>
+                <td>F</td>
+                <td>V</td>
+              </tr>
+              <tr>
+                <td>V</td>
+                <td>F</td>
+                <td>F</td>
+              </tr>
+            </tbody>
+          </table>
+
         <h3 class="titulo" id="tema-4"><?php echo $mat[4] ?></h3>
         <h3 class="titulo" id="tema-5"><?php echo $mat[5] ?></h3>
         <p class="texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
